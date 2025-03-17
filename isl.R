719d80b445fc4a4575390d419afe24c09001551a
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2025-03-18 02:30:02 +0530

    add ISL analysis

---
+++ isl.R
@@ -0,0 +1,194 @@
+library(pacman)
+p_load(tidyverse, readxl, BradleyTerry2, ggtext, showtext, ggpattern, systemfonts, grid)
+
+# Read in the data
+isl_raw <- read_excel("./data/Copy of isl_raw_data(1.9.9).xlsx")
+isl_raw2 <- isl_raw |> 
+  select(-time)
+
+is.na(isl_raw2) |> colSums() |> as.data.frame() |> View()
+
+# Clean the data
+atk_home_goals <- isl_raw2 |> 
+  filter(home_team == "Atletico de Kolkata") |>
+  summarise(total_home_goals = sum(home_team_score))
+  
+
+names(isl_raw2)
+dat <- isl_raw2[1:809, c(3,4,6,7)]
+
+standings <- dat |> 
+  pivot_longer(cols = c(home_team, away_team), 
+               names_to = "venue", values_to = "team") |> 
+  mutate(goals_scored = if_else(venue == "home_team", home_team_score, away_team_score),
+         goals_conceded = if_else(venue == "home_team", away_team_score, home_team_score),
+         points = case_when(goals_scored > goals_conceded ~ 3,  # Win
+                            goals_scored == goals_conceded ~ 1, # Draw
+                            TRUE ~ 0)) |> 
+  group_by(team) |> 
+  summarize(
+    matches_played = n(),
+    wins = sum(points == 3),
+    draws = sum(points == 1),
+    losses = sum(points == 0),
+    goals_scored = sum(goals_scored),
+    goals_conceded = sum(goals_conceded),
+    goal_difference = goals_scored - goals_conceded,
+    points = sum(points)
+  )
+
+
+results <- dat |> 
+  filter(away_team != 'M') |> 
+  mutate(
+    winner = ifelse(home_team_score > away_team_score, home_team,
+                    ifelse(away_team_score > home_team_score, away_team, "Draw")),
+    opponent = ifelse(winner == home_team, away_team,
+                      ifelse(winner == away_team, home_team, NA))) |> 
+  filter(winner != "Draw") |> 
+  select(winner, opponent) |> 
+  mutate(winner_fac = factor(winner, levels = unique(c(winner, opponent))),
+         opponent_fac = factor(opponent, levels = unique(c(winner, opponent))))
+
+
+# results$winner_fac <- factor(results$winner, levels = unique(c(results$winner, results$opponent)))
+# results$opponent_fac <- factor(results$opponent, levels = unique(c(results$winner, results$opponent)))
+
+
+
+# Fit the Bradley-Terry model
+model <- BTm(outcome = rep(1, nrow(results)), winner_fac, opponent_fac, data = results)
+
+# Summarize the model
+summary(model)
+
+# Extract coefficients
+coef_ATK <- 0  # Reference category ATK
+coef_NEFC <- coef(model)["..NorthEast United FC"]
+coef_HFC <- coef(model)["..Hyderabad FC"]
+coef_JFC <- coef(model)["..Jamshedpur FC"]
+coef_OFC <- coef(model)["..Odisha FC"]
+coef_BFC <- coef(model)["..Bengaluru FC"]
+coef_CFC <- coef(model)["..Chennaiyin FC"]
+coef_FCG <- coef(model)["..FC Goa"]
+coef_KBFC <- coef(model)["..Kerala Blasters FC"]
+coef_MCFC <- coef(model)["..Mumbai City FC"]
+coef_EBFC <- coef(model)["..East Bengal FC"]
+coef_MBSG <- coef(model)["..Mohun Bagan Super Giant"]
+
+
+
+# Calculate strength parameters
+strengths <- model$coefficients |> 
+  as.data.frame() |>
+  rownames_to_column(var = "Team") |>
+  add_row(Team = "..Atletico de Kolkata", `model$coefficients` = 0) |>
+  mutate(Strength = exp(`model$coefficients`),
+         Team = str_remove_all(Team, "\\..")) |> 
+  select(Team, Strength) |>
+  arrange(desc(Strength))
+
+# Display strength parameters
+print(strengths)
+
+# Probability that MBSG is preferred over JFC
+lambda_MBSG <- strengths["Mohun Bagan Super Giant"]
+lambda_JFC <- strengths["Jamshedpur FC"]
+prob_MBSG_over_JFC <- lambda_MBSG / (lambda_MBSG + lambda_JFC)
+print(prob_MBSG_over_JFC)
+
+font_add_google("Poppins", family = "poppins")
+font_add_google("Inter", family = "inter")
+showtext_auto()
+
+
+stack_patterns <- function(...) {
+  patterns <- list(...)
+  
+  # helper function to check for solid colours
+  is_valid_colour <- function(x) {
+    is(x, "character") &&
+      (x %in% colours() ||
+         grepl("^\\#[0-9a-fA-F]{6}$", x) ||
+         grepl("^\\#[0-9a-fA-F]{8}$", x))
+  }
+  
+  # check if any are not a pattern or colour
+  stopifnot(
+    "All supplied arguments must be patterns" =
+      patterns |>
+      sapply(\(x) is(x, "GridPattern") ||
+               is_valid_colour(x)) |>
+      all()
+  )
+  
+  # wrap each gradient in a grob
+  patterns |>
+    lapply(\(x) grid::rectGrob(gp = grid::gpar(fill = x))) ->
+    pattern_grobs
+  
+  # return as a compound pattern
+  grid::pattern(
+    do.call(grid::grobTree, pattern_grobs),
+    extend = "none")
+}
+
+
+
+
+# gradient_start <- c("#008000", "#0000FF", "#62daf3", "#008000",
+#                     "#df0e0e", "#ff8700", "#ef99eb", "#fdb006",
+#                     "#670c4c", "#df0e0e", "#008000", "#62daf3",
+#                     "#c66363", "#81cf0f")
+
+# gradient_end <- c("#a51532", "#fa1717", "#ffffff", "#008000",
+#                   "#df0e0e", "#ff8700", "#ef99eb", "#fdb006",
+#                   "#670c4c", "#fff702", "#a51532", "#0000FF",
+#                   "#c66363", "#81cf0f")
+
+# strengths <- strengths |> 
+#   mutate(gradient_start = gradient_start,
+#          gradient_end = gradient_end)
+
+# Plot the ranks
+isl_ranks <- ggplot(strengths, aes(x = reorder(Team, Strength), y = Strength, fill = Team)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_fill_manual(values = c("#ff0000", "#0000FF", "#063bfd", "#008000",
+                               "#df0e0e", "#ff8700", "#ef99eb", "#fdb006",
+                               "#670c4c", "#fff702", "#a51532", "#62daf3",
+                               "#c66363", "#81cf0f")) +
+  # geom_col_pattern(
+  #   aes(pattern_fill = gradient_start, pattern_fill2 = gradient_end),
+  #   pattern = "gradient",
+  #   pattern_angle = 0,
+  #   color = "black") +
+  coord_flip() +
+  labs(title = "ISL Teams' Strength Rankings Based on Bradley-Terry Model",
+       subtitle = "Analyzing Team Performance through Statistical Ranking",
+       x = "ISL Teams", 
+       y = "Strength",
+       caption = "Data collected till March 2023") +
+  theme_minimal() +
+  theme(text = element_text(family = "poppins"),
+        axis.text.y = element_text(size = 16, face = "bold"),
+        axis.text.x = element_text(size = 20),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        plot.title = element_text(size = 25, hjust = 0.5, face = "bold"),
+        plot.subtitle = element_text(size = 20, hjust = 0.5, face = "bold"),
+        plot.caption = element_text(size = 15),
+        legend.position = "none",
+        plot.background = element_rect(fill = "#fdd3d3", color = NA),
+        panel.background = element_rect(fill = "#c7faaf", color = NA),
+        panel.grid.minor = element_line(colour = "gray40")) +
+  geom_text(aes(label = round(Strength, 2)), hjust = -0.1, size = 6.5) +
+  geom_hline(yintercept = 0, color = "black", linewidth = 1)
+
+
+# Save the plot
+ggsave("ISL_Teams_Strength_Rankings.svg", isl_ranks,
+       dpi = 900, limitsize = T, width = 15, height = 10, units = "in")
+# ggsave("ISL_Teams_Strength_Rankings.png", isl_ranks,
+#        dpi = 900, limitsize = T, width = 15, height = 10, units = "in")
+
+  
---
+++ worldfootball.R
@@ -0,0 +1,32 @@
+library(pacman)
+p_load(tidyverse, readxl, worldfootballR)
+
+mapped_players <- player_dictionary_mapping()
+dplyr::glimpse(mapped_players)
+
+
+team_urls <- tm_league_team_urls(country_name = "England", start_year = 2020)
+# if it's not a league in the stored leagues data in worldfootballR_data repo:
+league_one_teams <- tm_league_team_urls(start_year = 2020, league_url = "https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3")
+
+team_urls_ind <- tm_league_team_urls(country_name = "India", start_year = 2024)
+isl_teams <- tm_league_team_urls(league_url = "https://www.transfermarkt.com/indian-super-league/startseite/wettbewerb/IND1", start_year = 2024)
+
+#----- for one team: -----#
+bayern <- tm_team_transfers(team_url = "https://www.transfermarkt.com/fc-bayern-munchen/startseite/verein/27/saison_id/2020", transfer_window = "all")
+dplyr::glimpse(bayern)
+
+#----- or for multiple teams: -----#
+# team_urls <- tm_league_team_urls(country_name = "England", start_year = 2020)
+# epl_xfers_2020 <- tm_team_transfers(team_url = team_urls, transfer_window = "all")
+
+mbsg <- tm_team_transfers(team_url = isl_teams[1], transfer_window = "all")
+mbsg
+
+mbsg_sq_stat <- tm_squad_stats(team_url = isl_teams[1])
+mbsg_sq_stat
+
+ebfc_sq_stat <- tm_squad_stats(team_url = isl_teams[4])
+ebfc_sq_stat
+
+
